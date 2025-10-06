e83217696a87feaea9bf92dc13fa00bb0ad4394a
Rejaurrahman5 <mdrejaurrahmanr@gmail.com>
2025-10-06 20:28:10 +0600

    Condition problemb

---
+++ script.js
@@ -0,0 +1,19 @@
+let age = 16;
+if(age >= 18){
+console.log("You can drive...");
+}
+else{
+    console.log("You cannot drive...")
+}
+
+
+let time = 12;
+if(time > 16){
+    console.log("Good Day");
+}
+else if (time < 10){
+    console.log("Good Morning");
+}
+else{
+    console.log("Ok")
+}
